a354ae196ae6fa191b52d8a5ebabcabedbeeb1fd
ajayvisu <103246613+ajayvisu@users.noreply.github.com>
2022-07-26 10:05:11 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,43 +1,7 @@
-# print("hello world")
-#
-# # a = "ajay"
-# # print(a)
-# #
-# # x,y,z = 1, 2.2, "hi"
-# # print(x, y, z)
-# # print(type(x))
-# # print(type(y))
-# # print(type(z))
-# #
-#
-# # data 18-07-22
-# # list
-# x=['a', 'b', 'c', 1, True, "a"]
-# print(type(x))
-# print(x)
-#
-# # IN AND NOT IN KEYWORDS
-# print('b' not in x)
-# print ("b" in x)
-#
-# print(x[3])
-# print(x[2:6])
-# print(x[-4:-1])
-#
-# list=["car",'van','bus','train']
-# list [1]='bike'
-# list[1:3] = ['no vehicals']
-# print((list))
-
-# list1=["car",'van','bus','train']
-# list2=['car1', 'car2']
-# list3= list1+list2
-# print(list3)
-# list4=list3.__delitem__(1)
-# print((list4))
-
-# Set
-set1={'a', 'b', 'c', 1, 2, 3}
-set2={4, 5, 6, 'z', 'y', 'x'}
-set1.add(set2)
-print(set1)
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hellow():
+    return "hellow"
+if __name__ == "--main__":
+    app.run(debug=True)
