f12f88e6bc9c325952a0efadff10fa2d106cdfd4
ajayvisu <103246613+ajayvisu@users.noreply.github.com>
2022-07-28 11:05:02 +0530

    Add files via upload

---
+++ numpy.py
@@ -0,0 +1,23 @@
+# # numeric python for array handling / multidimensional array / data science
+# import numpy as np
+#
+#
+#
+# a = np.array((1,2,3,4,5))
+# print(a)
+#
+# arr= np.array([1,2,3,4,], [1,2,3,4])
+#
+# arr1 = np.array([[1,2,3,4],[1,2,3,4]],[[1,2,3,4],[1,2,3,4]])
+
+arr = np.array([1,1,3,4])
+print(arr[3])
+
+arr= np.array([[1,2,3,4,], [1,2,3,4]])
+row1 = [1,2,3,4,] index = 0
+row2 = [1,2,3,4,] index = 1
+print(" 1 st row " arr[0,2])
+
+
+
+
